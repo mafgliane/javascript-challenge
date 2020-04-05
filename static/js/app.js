f8e41f62b734cdd4568e334f59f86b0c4062d4ae
mafgliane <mafgliane@gmail.com>
2020-04-05 14:30:42 -0700

    updated app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,6 @@
 // from data.js
 var tableData = data;
+
 //console.log(data)
 
 // YOUR CODE HERE!
@@ -9,18 +10,29 @@ var button = d3.select("#filter-btn");
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
+//Initialize the table to contain all data
+updateTable(tableData);
+
 //get filter criteria on button_click
 button.on("click", function() {
     //  Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
 
     // Get the value property of the input element
-    inputValue=checkDateFormat(inputElement.property("value"))
-    //console.log(inputValue)
-    fetchData(inputValue);
+    // inputValue=checkDateFormat(inputElement.property("value"))
+    inputValue=inputElement.property("value");
+    console.log(inputValue)
+    if(inputValue===""){
+        //clear filter to show all data
+        updateTable(tableData);
+    } else{
+        //show only data that matches the filter
+        checkDateFormat(inputValue);
+        fetchData(inputValue);
+    }
 });
 
-//Remove leading zero from mm-dd
+//Remove leading zero from mm-dd 
 function checkDateFormat(givenStr){
     var tokens = givenStr.split("/");
     var mm = tokens[0];
